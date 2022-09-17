ea32df570308b90407f64ba4303d2bb8352d6bf9
p5eudoDjnn <alexander.havers@gmail.com>
2022-09-17 14:19:57 -0400

    add functions to pass Manager tests

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -5,12 +5,12 @@ const Manager = require("../lib/Manager");
 test("creates the object", () => {
   const manager = new Manager("Bobby", 8, "lavahrexes@gmail.com", 1);
 
-  expect(manager.officeNumber()).toEqual(expect.any(Number));
+  expect(manager.officeNumber).toEqual(expect.any(Number));
 });
 
 // getRole() tests
 test("Gets role of Manager", () => {
-  const manager = new Manager("Bobby", 8, "lavahrexes@gmail.com", 1);
+  const manager = new Manager("Bobby", 8, "lavahrexes@gmail.com");
 
   expect(manager.getRole()).toEqual("Manager");
 });
